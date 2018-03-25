03fff58fa90b7fc2a795ee745e52d2fc6431610a
Ricardo Hernández <ricardohs35@gmail.com>
2018-03-25 16:53:25 -0600

    🎨 Change the old postCount feature and add likes instead

--- users/test/update_test.js
+++ users/test/update_test.js
@@ -5,7 +5,7 @@ describe('Updating records', () => {
   let joe;
 
   beforeEach((done) => {
-    joe = new User({ name: 'Joe', postCount: 0 });
+    joe = new User({ name: 'Joe', likes: 0 });
     joe.save()
       .then(() => done());
   });
@@ -50,12 +50,12 @@ describe('Updating records', () => {
     );
   });
 
-  xit('A user can have their postCount incremented by 1', (done) => {
-    User.update({ name: 'Joe' }, { $inc: { postCount: 1 } })
+  it('A user can have their postCount incremented by 1', (done) => {
+    User.update({ name: 'Joe' }, { $inc: { likes: 10 } })
       .then(() => User.findOne({ name: 'Joe' }))
       .then(user => {
-        assert(user.postCount === 1);
-        done();
+        assert(user.likes === 10)
+        done()
       })
-  });
-});
+  })
+})
