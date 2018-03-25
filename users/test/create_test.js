e658a019441fef5b382b9c4b77977314b9e03917
Ricardo Hernández <ricardohs35@gmail.com>
2018-03-25 11:52:25 -0600

    🎨 Update schema and add some testing

--- users/test/create_test.js
+++ users/test/create_test.js
@@ -1,1 +1,15 @@
-const assert = require('assert');const User = require('../src/user');describe('Creating records', () => {  it('Saves a user', (done) => {    const joe = new User({ name: 'Joe' });    joe.save()      .then(() => {        // Has joe been saved successfully?        assert(!joe.isNew);        done();      });  });});
+const assert = require('assert');
+const User = require('../src/user');
+
+describe('Creating records', () => {
+  it('Saves a user', (done) => {
+    const joe = new User({ name: 'Joe' });
+
+    joe.save()
+      .then(() => {
+        // Has joe been saved successfully?
+        assert(!joe.isNew);
+        done();
+      });
+  });
+});
