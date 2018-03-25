2723af9eae6c08151b14b19f809981e8ab78d8df
Ricardo Hernández <ricardohs35@gmail.com>
2018-03-25 12:09:03 -0600

    🎨 Fix line breaks

--- users/test/test_helper.js
+++ users/test/test_helper.js
@@ -1,1 +1,19 @@
-const mongoose = require('mongoose');mongoose.Promise = global.Promise;before((done) => {  mongoose.connect('mongodb://localhost/users_test', {    useMongoClient: true  });  mongoose.connection    .once('open', () => done())    .on('error', (error) => console.warn('Warning', error))  ;});beforeEach((done) => {  mongoose.connection.collections.users.drop(() => {    // Ready to run the next test!    done();  });});
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+
+before((done) => {
+  mongoose.connect('mongodb://localhost/users_test', {
+    useMongoClient: true
+  });
+  mongoose.connection
+    .once('open', () => done())
+    .on('error', (error) => console.warn('Warning', error))
+  ;
+});
+
+beforeEach((done) => {
+  mongoose.connection.collections.users.drop(() => {
+    // Ready to run the next test!
+    done();
+  });
+});
