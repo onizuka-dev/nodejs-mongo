878f7a2636049afed589462dbb5e75575428e4d1
Ricardo Hernández <ricardohs35@gmail.com>
2018-03-25 16:48:26 -0600

    💩 Skip old postCount test

--- users/test/update_test.js
+++ users/test/update_test.js
@@ -50,7 +50,7 @@ describe('Updating records', () => {
     );
   });
 
-  it('A user can have their postCount incremented by 1', (done) => {
+  xit('A user can have their postCount incremented by 1', (done) => {
     User.update({ name: 'Joe' }, { $inc: { postCount: 1 } })
       .then(() => User.findOne({ name: 'Joe' }))
       .then(user => {
