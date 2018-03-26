3d858831b7474b66f1143c191db3fda008e1a40e
Ricardo Hernández <ricardohs35@gmail.com>
2018-03-25 23:01:24 -0600

    🚧 Setup relations tests

--- users/test/association_test.js
+++ users/test/association_test.js
@@ -14,5 +14,20 @@ describe('Associations', () => {
     joe.blogPosts.push(blogPost)
     blogPost.comments.push(comment)
     comment.user = joe
+
+    Promise.all([
+      joe.save(),
+      blogPost.save(),
+      comment.save()
+    ])
+      .then(() => done())
+  })
+
+  it.only('Saves a relation between a user and a blogpost', (done) => {
+    User.findOne({ name: 'Joe' })
+      .then((user) => {
+        console.log(user)
+        done()
+      })
   })
 })
